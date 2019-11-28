19aaa8e2b6e62faafa30af130b52f9610c641bcb
Koki Takahashi <hakatasiloving@gmail.com>
2019-11-29 08:50:29 +0900

    Import working version of mozjs example into code

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,22 +1,27 @@
+extern crate neon;
+
+#[macro_use]
+extern crate mozjs;
+
 use neon::prelude::*;
+use mozjs::jsapi::CompartmentOptions;
 use mozjs::jsapi::JS_NewGlobalObject;
 use mozjs::jsapi::OnNewGlobalHookOption;
 use mozjs::jsval::UndefinedValue;
-use mozjs::rust::{JSEngine, RealmOptions, Runtime, SIMPLE_GLOBAL_CLASS};
+use mozjs::rust::{JSEngine, Runtime, SIMPLE_GLOBAL_CLASS};
 
 use std::ptr;
 
 fn hello(mut c: FunctionContext) -> JsResult<JsString> {
     let engine = JSEngine::init().unwrap();
-    let rt = Runtime::new(engine.handle());
+    let rt = Runtime::new(engine);
     let cx = rt.cx();
 
     unsafe {
-        let options = RealmOptions::default();
         rooted!(in(cx) let global =
             JS_NewGlobalObject(cx, &SIMPLE_GLOBAL_CLASS, ptr::null_mut(),
                                OnNewGlobalHookOption::FireOnNewGlobalHook,
-                               &*options)
+                               &CompartmentOptions::default())
         );
         rooted!(in(cx) let mut rval = UndefinedValue());
         assert!(rt.evaluate_script(global.handle(), "1 + 1",
@@ -28,3 +33,4 @@ fn hello(mut c: FunctionContext) -> JsResult<JsString> {
 register_module!(mut cx, {
     cx.export_function("hello", hello)
 });
+
