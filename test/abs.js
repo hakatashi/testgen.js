2dd89d397686a2659c75712abcce082c7a320c53
Koki Takahashi <hakatasiloving@gmail.com>
2019-12-16 06:37:29 +0900

    Convert indentations to tabs

--- test/abs.js
+++ test/abs.js
@@ -1,9 +1,9 @@
 const abs = (n) => {
-  if (n > 0) {
-    return n;
-  }
-  if (n < 0) {
-    return -n;
-  }
-  return 0;
+	if (n > 0) {
+		return n;
+	}
+	if (n < 0) {
+		return -n;
+	}
+	return 0;
 };
