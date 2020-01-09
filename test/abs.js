a0863857d5b576fd768358f681ba90a527414c6e
Koki Takahashi <hakatasiloving@gmail.com>
2020-01-10 01:51:50 +0900

    Fix indentation of test/abs.js

--- test/abs.js
+++ test/abs.js
@@ -3,15 +3,15 @@ const {constructGraph} = require('../lib/analyzer.js');
 const {inspect} = require('util');
 
 (async () => {
-const codes = await compileFunction((n) => {
-	if (n > 0) {
-		return n;
-	}
-	if (n < 0) {
-		return -n;
-	}
-	return 0;
-});
+	const codes = await compileFunction((n) => {
+		if (n > 0) {
+			return n;
+		}
+		if (n < 0) {
+			return -n;
+		}
+		return 0;
+	});
 
-console.log(inspect(constructGraph(codes), {depth: null}));
+	console.log(inspect(constructGraph(codes), {depth: null}));
 })();
