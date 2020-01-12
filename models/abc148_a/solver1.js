a34b081894ce025efe8d1fcf7b5316fdec8ce5c3
Koki Takahashi <hakatasiloving@gmail.com>
2020-01-12 12:01:08 +0900

    Swap solver1 and solver2

--- models/abc148_a/solver1.js
+++ models/abc148_a/solver1.js
@@ -1,1 +1,22 @@
-module.exports = (x, y) => 6 - (x + y);
+module.exports = (x, y) => {
+	if (x === 1) {
+		if (y === 2) {
+			return 3;
+		} else if (y === 3) {
+			return 2;
+		}
+	} else if (x === 2) {
+		if (y === 1) {
+			return 3;
+		} else if (y === 3) {
+			return 1;
+		}
+	} else if (x === 3) {
+		if (y === 1) {
+			return 2;
+		} else if (y === 2) {
+			return 1;
+		}
+	}
+	return 0;
+};
